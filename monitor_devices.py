b04bb11cea2816360e2e300365cd2744ecd62fdf
James Blanton <jamespblanton@gmail.com>
2022-07-26 21:20:45 -0400

    read and print to console

---
+++ monitor_devices.py
@@ -0,0 +1,12 @@
+import board
+import adafruit_ahtx0
+import time
+
+# import paho mqtt
+
+sensor = adafruit_ahtx0.AHTx0(board.I2C())
+
+while True:
+    print("\nTemperature: %0.1f C" % sensor.temperature)
+    print("Humidity: %0.1f %%" % sensor.relative_humidity)
+    time.sleep(2)
