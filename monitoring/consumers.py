26c427210983b5db26670bc1c1bd471a970d9085
James Blanton <jamespblanton@gmail.com>
2023-01-24 14:14:40 -0500

    added consumer info for devices

--- monitoring/consumers.py
+++ monitoring/consumers.py
@@ -7,7 +7,7 @@ import django
 from django.conf import settings
 from django.db import IntegrityError
 
-from .models import SensorHost, SensorData, SensorMetric, MQTTMessage
+from .models import SensorHost, SensorData, SensorMetric, MQTTMessage, Device, Status
 
 
 logger = logging.getLogger(__name__)
@@ -38,8 +38,10 @@ class mqttConsumer(MqttConsumer):
         # maybe a celery task that runs when lux is updated
         # checking light level against on/off status of device
         match mqtt_message["topic"].split("/"):
-            case [place, device, "temperature" | "humidity" as metric]:
+            case [place, device, "temperature" | "humidity" | "lux" as metric]:
                 await self.insert_measure(payload, place, device, metric)
+            case [place, device, "status" as metric]:
+                await self.update_status(payload, place, device, metric)
             case [place, device, "error" | "warning" as issue]:
                 pass
             case _:
@@ -60,6 +62,17 @@ class mqttConsumer(MqttConsumer):
         await asyncio.wait(unsubs)
 
     @database_sync_to_async
+    def update_status(self, payload: str | bytes, place: str, device: str, metric: str):
+        full_name = "_".join([place, device])
+        if payload == b"True":
+            status = "on"
+        elif payload == b"False":
+            status = "off"
+        obj, obj_created = Device.objects.update_or_create(
+            name=full_name, status=status
+        )
+
+    @database_sync_to_async
     def insert_measure(
         self, payload: str | bytes, place: str, device: str, metric: str
     ) -> None:
@@ -69,8 +82,8 @@ class mqttConsumer(MqttConsumer):
         # debug for each data insert
         host_obj, host_created = SensorHost.objects.get_or_create(host=host)
         metric_obj, metric_created = SensorMetric.objects.get_or_create(metric=metric)
-        if not host_created:
-            print("host already exists")
-        if not metric_created:
-            print("metric already exists")
+        if host_created:
+            logger.info("New host created: {}".format(host_obj.host))
+        if metric_created:
+            logger.info("New metric created: {}".format(metric_obj.metric))
         SensorData.objects.create(host=host_obj, metric=metric_obj, data=value)
