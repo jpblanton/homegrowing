7a437b9597d2cdb273a16e918fd6219a0d905809
James Blanton <jamespblanton@gmail.com>
2023-01-31 15:23:58 -0500

    routing tweak

--- config/asgi.py
+++ config/asgi.py
@@ -11,23 +11,20 @@ import os
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
+from django.urls import path
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 
 django_asgi_app = get_asgi_application()
 
-from monitoring.consumers import mqttConsumer
-import monitoring.routing
+from monitoring.consumers import mqttConsumer, StreamConsumer
 
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
         # mqtt handler
         "mqtt": mqttConsumer.as_asgi(),
-        "websocket": AuthMiddlewareStack(
-            URLRouter(monitoring.routing.websocket_urlpatterns)
-        ),
+        'websocket': URLRouter([path('practice', StreamConsumer.as_asgi())])
     }
-)
+
