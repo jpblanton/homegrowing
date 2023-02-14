1d3acb10459ab54dfdb077057a24d3e34d5c276e
James Blanton <jamespblanton@gmail.com>
2023-02-14 14:42:58 -0500

    created admin models for better viewing

--- monitoring/admin.py
+++ monitoring/admin.py
@@ -9,9 +9,39 @@ from monitoring.models import (
     Device,
 )
 
-admin.site.register(SensorMetric)
-admin.site.register(SensorData)
-admin.site.register(SensorHost)
-admin.site.register(GrowthStage)
-admin.site.register(GrowthStageHistory)
-admin.site.register(Device)
+
+class GrowthStageAdmin(admin.ModelAdmin):
+    pass
+
+
+class GrowthStageHistoryAdmin(admin.ModelAdmin):
+    list_display = ["growth_stage", "created_at"]
+    readonly_fields = ["growth_stage", "created_at"]
+
+
+class SensorDataAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_at"
+    list_display = ["host", "metric", "data", "created_at"]
+    readonly_fields = ["host", "metric", "data", "created_at"]
+    ordering = ["-created_at"]
+    list_filter = ("host", "metric", "created_at")
+
+
+class SensorHostAdmin(admin.ModelAdmin):
+    pass
+
+
+class SensorMetricAdmin(admin.ModelAdmin):
+    pass
+
+
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ["name", "status", "category", "updated_at"]
+
+
+admin.site.register(SensorMetric, SensorMetricAdmin)
+admin.site.register(SensorData, SensorDataAdmin)
+admin.site.register(SensorHost, SensorHostAdmin)
+admin.site.register(GrowthStage, GrowthStageAdmin)
+admin.site.register(GrowthStageHistory, GrowthStageHistoryAdmin)
+admin.site.register(Device, DeviceAdmin)
